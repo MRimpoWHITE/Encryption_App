4f19ce190d96d4e135fa350818b7f80ccef49c48
/GaiTod✅ <phichitphu2015@gmail.com>
2025-09-16 01:14:30 +0700

    update_main and built program
    สร้างโปรแกรมเสร็จแย้ว

--- project_encrytion.py
+++ project_encrytion.py
@@ -159,6 +159,8 @@ class AESFileEncryptorApp(ctk.CTk):
             self.text_output.insert("1.0", f"Error: {str(e)}")
             self.status_label.configure(text="There is an Error with text mode", text_color="red")
             
+#================ ฟังค์ชั่นถอดรหัสข้อความ ====================================================================
+            
     def do_text_decrypt(self):
         try:
             ciphertext_hex = self.text_input.get("1.0", "end-1c").strip()
@@ -228,6 +230,8 @@ class AESFileEncryptorApp(ctk.CTk):
             self.status_label.configure(text="There is an Error in File Mode", text_color="red")
             messagebox.showerror("Error Found", str(e))
             
+#================ ฟังค์ชั่นถอดรหัสไฟล์ ====================================================================
+            
     def do_file_decrypt(self):
         try:
             input_path = self.file_input_entry.get().strip()
@@ -245,14 +249,24 @@ class AESFileEncryptorApp(ctk.CTk):
             # ถอดรหัสไฟล์
             decrypted_data = decrypt(ciphertext, key)
             
-            # สร้าง output path (เติม _decrypted ข้างหลัง)
-            base, ext = os.path.splitext(input_path)
-            output_path = f"{base}_decrypted{ext}"
+            # # สร้าง output path (เติม _decrypted ข้างหลัง)
+            # base, ext = os.path.splitext(input_path)
+            # output_path = f"{base}_decrypted{ext}"
             
             # # สร้าง output path (เติม decrypted_ ข้างหน้า)
             # dirname = os.path.dirname(input_path)
             # filename = os.path.basename(input_path) 
-            # output_path = os.path.join(dirname, f"Decrypted_{filename}")            
+            # output_path = os.path.join(dirname, f"Decrypted_{filename}")      
+            
+            dirname = os.path.dirname(input_path)
+            filename = os.path.basename(input_path)
+            #แยกชื่อไฟล์และนามสกุล
+            base, ext = os.path.splitext(filename)
+            # ลบ _encrypted ถ้ามี
+            if base.endswith('_encrypted'):
+                base = base[:-len('_encrypted')]
+            # เติม _decrypted
+            output_path = os.path.join(dirname, f"{base}_decrypted{ext}")      
             
             # บันทึกไฟล์
             with open(output_path, 'wb') as f:
